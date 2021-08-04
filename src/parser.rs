74bd4fc6a7d31e0f0dba157e1d68dbb696760464
Ofek Shochat <o@shoch.at>
2021-08-04 19:40:38 +0200

    removing unused include

--- src/parser.rs
+++ src/parser.rs
@@ -3,7 +3,6 @@ use lexer::Token;
 use crate::token_kinds::{BinOp::*, Literal::*, TokenKind::*};
 use crate::token_kinds::TokenKind;
 use std::fmt;
-use std::ops::Index;
 use std::process::exit;
 use std::str::from_utf8;
 
